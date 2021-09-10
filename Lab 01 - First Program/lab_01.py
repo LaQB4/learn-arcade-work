7053d9791a04d21b79ef8714b7063244216b8c54
LaQB4 <89827612+LaQB4@users.noreply.github.com>
2021-09-10 13:58:24 -0700

    Add files via upload

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,13 @@
+print("This is one of my first python projects")
+print("""Hopefully I'll get to:
+make a couple games
+be able to make stuff in my free time
+be comfortable using the terminal on mac""")
+print("""░░░░█░▀▄░░░░░░░░░░▄▄███▀░░
+░░░░█░░░▀▄░▄▄▄▄▄░▄▀░░░█▀░░
+░░░░░▀▄░░░▀░░░░░▀░░░▄▀░░░░
+░░░░░░░▌░▄▄░░░▄▄░▐▀▀░░░░░░
+░░░░░░▐░░█▄░░░▄█░░▌▄▄▀▀▀▀█
+░░░░░░▌▄▄▀▀░▄░▀▀▄▄▐░░░░░░█
+░░░▄▀▀▐▀▀░░░░░░░▀▀▌▄▄▄░░░█
+░░░█░░░▀▄░░░░░░░▄▀░░░░█▀▀▀""")
